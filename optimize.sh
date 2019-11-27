e86804bbb99b74b4d8975d7dbe271729c5aeac2a
Asur <asur@asurbernardo.com>
2019-11-27 12:06:39 +0100

    Update amp-optimizer job version

--- optimize.sh
+++ optimize.sh
@@ -1,6 +1,6 @@
 for file in $(find public -name "*.html"); do
     [ -f "$file" ] || break
     echo "Optimizing $file" 
-    $GOPATH/bin/transform $file > /tmp/optimized.txt
+    ! $GOPATH/bin/transform $file > /tmp/optimized.txt
     [ -s /tmp/optimized.txt ] && cat /tmp/optimized.txt > $file || echo "Not a valid AMP page. Omitting..."
 done
